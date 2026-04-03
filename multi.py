1ae2781b522f616cb48ab284a9298c94ed3bd4ca
Httpslakshya <lakshyadharkar@gmail.com>
2026-04-03 23:25:18 +0530

    second commit

---
+++ multi.py
@@ -0,0 +1,13 @@
+from multiprocessing import process
+import time
+def brew_coffee():
+    print("brewing the coffee")
+    time.sleep(6)
+    print("coffee brewed...")
+if __name__ == "__main__":
+    chaimaker=[process(target=brew_coffee) for i in range(3)]
+    for maker in chaimaker:
+        maker.start()
+    for maker in chaimaker:
+        maker.join()
+        
