af5405ab01edff19d0a55fbc6d1a38b5bd2b4bab
Httpslakshya <lakshyadharkar@gmail.com>
2026-04-08 21:40:40 +0530

    update

--- multi.py
+++ multi.py
@@ -1,13 +1,12 @@
-from multiprocessing import process
+from multiprocessing import Process
 import time
 def brew_coffee():
     print("brewing the coffee")
     time.sleep(6)
     print("coffee brewed...")
 if __name__ == "__main__":
-    chaimaker=[process(target=brew_coffee) for i in range(3)]
+    chaimaker=[Process(target=brew_coffee) for i in range(3)]
     for maker in chaimaker:
         maker.start()
     for maker in chaimaker:
         maker.join()
-        
