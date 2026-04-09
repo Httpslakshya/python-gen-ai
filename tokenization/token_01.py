08dfb8b54e89ad70ca473911a5a9d343d1e1fbbe
Httpslakshya <lakshyadharkar@gmail.com>
2026-04-09 18:08:11 +0530

    token creation

---
+++ tokenization/token_01.py
@@ -0,0 +1,9 @@
+import tiktoken
+
+enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+text = "hello , i am lakshya dharkar"
+tokens=enc.encode(text)
+print("input tokens are:", tokens)
+print("after decoding token :",enc.decode(tokens))
+print("the length of token :",len(tokens))
