56620403e5d56323b75ef9bec9df7057d8b09ec3
Httpslakshya <lakshyadharkar@gmail.com>
2026-04-10 23:57:05 +0530

    implementing google genai model through api

---
+++ tokenization/Google_gemini.py
@@ -0,0 +1,13 @@
+from google import genai 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key =os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key)
+response= client.models.generate_content(
+    model="gemini-2.5-flash",contents="Explain how ai work in summary"
+
+)
+print(response.text)
