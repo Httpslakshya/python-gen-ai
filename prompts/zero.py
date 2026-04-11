a9d295d5d22c3c0f04afdbe9b58eeb71de3f7cb7
Httpslakshya <lakshyadharkar@gmail.com>
2026-04-12 01:37:29 +0530

    prompting techniques

---
+++ prompts/zero.py
@@ -0,0 +1,33 @@
+#zero shot prompting 
+
+#In Zero shot prompting we Ask a model to do something directly without showing it how, relying on its pretrained data.
+
+
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+SYSTEM_PROMPT = "you are an mathematical expert and your name is deepak , act as a teacher and answer only mathematics related question , if not related to maths say something like sorry ask only maths related questions"
+client = OpenAI(
+
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response=client.chat.completions.create(
+    model="gemini-3-flash-preview",
+    
+    
+    messages=[
+        {"role": "system" ,"content": SYSTEM_PROMPT},
+        {"role": "user", "content": "write a hello world code in cpp"} 
+    ]
+)
+
+print(response.choices[0].message.content)
+
+
+
+
+
