f9a6c6d2f2dcb94d53d37651819db94c75c358a5
Httpslakshya <lakshyadharkar@gmail.com>
2026-05-07 12:27:17 +0530

    vector embedding working

--- Agentic_Ai/RAG/index.py
+++ Agentic_Ai/RAG/index.py
@@ -1,9 +1,42 @@
 from langchain_community.document_loaders import PyPDFLoader
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_qdrant import QdrantVectorStore
 
-pdf_path = Path(__file__).parent / "ramayan-eng.pdf"
 
+load_dotenv()
+QDRANT_API_KEY=os.getenv("QDRANT_API_KEY")
+QDRANT_URL=os.getenv("QDRANT_URL")
+pdf_path = Path(__file__).parent / "pythonguide.pdf"
 
+#load the file in python program
 loader = PyPDFLoader(pdf_path)
 docs = loader.load()
-print(docs[25])
+#spilt the docs into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size =1000,
+    chunk_overlap=400
+)
+
+chunks=text_splitter.split_documents(documents=docs)
+
+#vector embedding
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vector_store = QdrantVectorStore.from_documents(
+    force_recreate=True,
+    documents=chunks,
+    embedding=embedding_model,
+    url=QDRANT_URL,
+    collection_name="agentic_ai_ud",
+    prefer_grpc=True,
+    api_key=QDRANT_API_KEY,
+)
+
+print("indexing of document done")
+
