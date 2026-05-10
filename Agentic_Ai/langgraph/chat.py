983217c22769b9fe2b721782e2d5a383f48df106
Httpslakshya <lakshyadharkar@gmail.com>
2026-05-10 20:46:37 +0530

    langgraph graph working demo

--- Agentic_Ai/langgraph/chat.py
+++ Agentic_Ai/langgraph/chat.py
@@ -7,23 +7,25 @@ class State(TypedDict):
     messages:Annotated[list,add_messages]
 
 def chatbot(state:State):
-    print("this is inside from chatbot node ,state :",state)
+    print("\n\n this is inside from chatbot node ,state :",state)
     return {"messages":["hi this is a messge from chatbot node"]}
 
 def samplenode(state:State):
 
-    print("this is inside from samplenode node ,state :",state)
+    print("\n\n this is inside from samplenode node ,state :",state)
     return{"messages":["these is from sample node"]}
 
 
 graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot",chatbot) #here we register chatbot function as a node, and also we named it chatbot.
+graph_builder.add_node("samplenode",samplenode)
 graph_builder.add_edge(START,"chatbot")
 graph_builder.add_edge("chatbot","samplenode")
 graph_builder.add_edge("samplenode",END)
 
 graph=graph_builder.compile()
-
+updated_state= graph.invoke(State({"messages":["Hi, my name is lakshya"]})) #you need to pass initial state when you invoke the graph
+print("\n\n updated state :",updated_state)
 
 #(START) -> chatbot -> samplenode -> (END) 
 
